2a33c30d16c7e31b5e623aa448423b9a888f4b67
IAmLex <lexkrooswijk@live.nl>
2020-10-06 23:08:11 +0200

    init

---
+++ src/main/java/com/lex/ips3backend/Ips3backendApplication.java
@@ -0,0 +1,13 @@
+package com.lex.ips3backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ips3backendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ips3backendApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/lex/ips3backend/models/User.java
@@ -0,0 +1,15 @@
+package com.lex.ips3backend.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+
+}
